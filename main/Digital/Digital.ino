a1f40ea99c7855b67badb8e2239fd7899e411f68
efranken <efrankie@gmail.com>
2020-10-07 20:45:01 -0400

    Trying digital pins out

--- main/Digital/Digital.ino
+++ main/Digital/Digital.ino
@@ -1,13 +1,17 @@
 // I'm trying to figure this whole thing out myself without looking anything up
 // This likely isn't the most logical or best practices way of doing it
+// Copied from main.sh
+// Trying digital out
 
 // Brown +
 // Blue -
 // Black interrupt
 
-int analogPin = A3;
+//int analogPin = A3;
 
-int val = 0;
+//int val = 0;
+
+//int digitalPin = D12;
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,9 +20,11 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  val = analogRead(analogPin);
 
-  Serial.println(val);
+  Serial.println(digitalRead(12));
+  //val = analogRead(analogPin);
+
+  //Serial.println(val);
 
   //if (val < 4) {
   //  Serial.println("Detect Magnet");
